9ecb3d03bf888db7c3cdcc767de8a337dd90147c
John Doe <sforde6776@gmail.com>
2023-11-04 19:03:01 -0400

    Profile

--- client/src/pages/Profile/StudentProfile.jsx
+++ client/src/pages/Profile/StudentProfile.jsx
@@ -1,7 +1,10 @@
 import "./StudentProfile.css";
 import homePhoto from "./home.png"
 import networkPhoto from "./network.png"
-import profilePhoto from "./profile.jpg"
+import profilePhoto from "./profile.png"
+import collegePhoto from "./collegeStudent.jpeg"
+import collegeBanner from "./collegeBannerPhoto.jpg"
+import { Link } from "react-router-dom";
 
 const StudentProfile = () => {
   return (
@@ -11,16 +14,37 @@ const StudentProfile = () => {
   );
 };
 
-function About() {
+function About({user}) {
   return (
     <>
-      <div className="w-75 topnav">
-        <img src={homePhoto} className="photos"/>
-        <input className="name" defaultValue="Name"/>
-        <input className="name" defaultValue="Name"/>
-        <input className="name" defaultValue="Name"/>
-        <img src={networkPhoto} className="photos"/>
-        <img src={profilePhoto} className="photos"/>
+      <div className="w-auto topnav">
+        <Link to={"/"} className="link"><img src={homePhoto} className="photos"/></Link>
+        <input className="fields" placeholder="Name"/>
+        <select className="fields"> 
+          <option value="BC"> Boston College </option>
+          <option value="BU"> Boston University </option>
+          <option value=""> Northeastern </option>
+        </select>
+        <Link to={""} className="link"> <button className="fields"> Search </button> </Link>
+        <img src={profilePhoto} className="endPhotos"/>
+        <img src={networkPhoto} className="endPhotos"/>
+      </div>
+      <div className="h-100 content">
+        <img className="top" src={collegeBanner}/>
+          <img src={collegePhoto} className="profileImage"/>
+        <div className="h-50 bottom">
+          <span className="w-25 basic">
+            <p> Jane Doe </p>
+            <p> doeja@bc.edu </p>
+            <p> Computer Science </p>
+          </span>
+          <span className="w-25 schools">
+            <p> Interested Schools </p>
+          </span>
+          <span className="w-25 preferences">
+            <p> School Preferences </p>
+          </span>
+        </div>
       </div>
     </>
   );
