5675c8231971bd07f58c8c92a674b027bf104ec7
Lilly Ohr <lillyohr@gmail.com>
2023-11-04 14:19:35 -0400

    Set up home page

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <main className="home-main">
+      <About />
+    </main>
+  );
+};
+
+function About() {
+  return (
+    <>
+      <h1>High School Networking App</h1>
+    </>
+  );
+}
+
+export default Home;
